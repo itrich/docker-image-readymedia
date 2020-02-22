ec962836bf39dd8d4c3f4f19634efd971fab05b0
Yehor Popovych <yehor@crossroad.io>
2020-02-22 23:31:48 +0200

    removed non-needed options from conf. Better start script

--- start.sh
+++ start.sh
@@ -1,155 +1,167 @@
 #!/bin/sh
 
-echo "port=${TCP_PORT}" >> /etc/minidlna.conf
-echo "friendly_name=${FRIENDLY_NAME}" >> /etc/minidlna.conf
-echo "serial=${SERIAL}" >> /etc/minidlna.conf
+if [[ ! -f "/cache/minidlna.conf" ]] then
+  cp /etc/minidlna.conf /cache/
+fi
+
+cp -f /cache/minidlna.conf /tmp/minidlna.conf
+
+echo "port=${TCP_PORT}" >> /tmp/minidlna.conf
+echo "friendly_name=${FRIENDLY_NAME}" >> /tmp/minidlna.conf
+echo "serial=${SERIAL}" >> /tmp/minidlna.conf
 
 # All media dirs
 if [[ ! -z "${ALL_MEDIA_DIR1}" ]]; then
-  echo "media_dir=${ALL_MEDIA_DIR1}" >> /etc/minidlna.conf
+  echo "media_dir=${ALL_MEDIA_DIR1}" >> /tmp/minidlna.conf
 fi
 
 if [[ ! -z "${ALL_MEDIA_DIR2}" ]]; then
-  echo "media_dir=${ALL_MEDIA_DIR2}" >> /etc/minidlna.conf
+  echo "media_dir=${ALL_MEDIA_DIR2}" >> /tmp/minidlna.conf
 fi
 
 if [[ ! -z "${ALL_MEDIA_DIR3}" ]]; then
-  echo "media_dir=${ALL_MEDIA_DIR3}" >> /etc/minidlna.conf
+  echo "media_dir=${ALL_MEDIA_DIR3}" >> /tmp/minidlna.conf
 fi
 
 if [[ ! -z "${ALL_MEDIA_DIR4}" ]]; then
-  echo "media_dir=${ALL_MEDIA_DIR4}" >> /etc/minidlna.conf
+  echo "media_dir=${ALL_MEDIA_DIR4}" >> /tmp/minidlna.conf
 fi
 
 if [[ ! -z "${ALL_MEDIA_DIR5}" ]]; then
-  echo "media_dir=${ALL_MEDIA_DIR5}" >> /etc/minidlna.conf
+  echo "media_dir=${ALL_MEDIA_DIR5}" >> /tmp/minidlna.conf
 fi
 
 if [[ ! -z "${ALL_MEDIA_DIR6}" ]]; then
-  echo "media_dir=${ALL_MEDIA_DIR6}" >> /etc/minidlna.conf
+  echo "media_dir=${ALL_MEDIA_DIR6}" >> /tmp/minidlna.conf
 fi
 
 if [[ ! -z "${ALL_MEDIA_DIR7}" ]]; then
-  echo "media_dir=${ALL_MEDIA_DIR7}" >> /etc/minidlna.conf
+  echo "media_dir=${ALL_MEDIA_DIR7}" >> /tmp/minidlna.conf
 fi
 
 if [[ ! -z "${ALL_MEDIA_DIR8}" ]]; then
-  echo "media_dir=${ALL_MEDIA_DIR8}" >> /etc/minidlna.conf
+  echo "media_dir=${ALL_MEDIA_DIR8}" >> /tmp/minidlna.conf
 fi
 
 if [[ ! -z "${ALL_MEDIA_DIR9}" ]]; then
-  echo "media_dir=${ALL_MEDIA_DIR9}" >> /etc/minidlna.conf
+  echo "media_dir=${ALL_MEDIA_DIR9}" >> /tmp/minidlna.conf
 fi
 
 # Video
 if [[ ! -z "${VIDEO_DIR1}" ]]; then
-  echo "media_dir=V,${VIDEO_DIR1}" >> /etc/minidlna.conf
+  echo "media_dir=V,${VIDEO_DIR1}" >> /tmp/minidlna.conf
 fi
 
 if [[ ! -z "${VIDEO_DIR2}" ]]; then
-  echo "media_dir=V,${VIDEO_DIR2}" >> /etc/minidlna.conf
+  echo "media_dir=V,${VIDEO_DIR2}" >> /tmp/minidlna.conf
 fi
 
 if [[ ! -z "${VIDEO_DIR3}" ]]; then
-  echo "media_dir=V,${VIDEO_DIR3}" >> /etc/minidlna.conf
+  echo "media_dir=V,${VIDEO_DIR3}" >> /tmp/minidlna.conf
 fi
 
 if [[ ! -z "${VIDEO_DIR4}" ]]; then
-  echo "media_dir=V,${VIDEO_DIR4}" >> /etc/minidlna.conf
+  echo "media_dir=V,${VIDEO_DIR4}" >> /tmp/minidlna.conf
 fi
 
 if [[ ! -z "${VIDEO_DIR5}" ]]; then
-  echo "media_dir=V,${VIDEO_DIR5}" >> /etc/minidlna.conf
+  echo "media_dir=V,${VIDEO_DIR5}" >> /tmp/minidlna.conf
 fi
 
 if [[ ! -z "${VIDEO_DIR6}" ]]; then
-  echo "media_dir=V,${VIDEO_DIR6}" >> /etc/minidlna.conf
+  echo "media_dir=V,${VIDEO_DIR6}" >> /tmp/minidlna.conf
 fi
 
 if [[ ! -z "${VIDEO_DIR7}" ]]; then
-  echo "media_dir=V,${VIDEO_DIR7}" >> /etc/minidlna.conf
+  echo "media_dir=V,${VIDEO_DIR7}" >> /tmp/minidlna.conf
 fi
 
 if [[ ! -z "${VIDEO_DIR8}" ]]; then
-  echo "media_dir=V,${VIDEO_DIR8}" >> /etc/minidlna.conf
+  echo "media_dir=V,${VIDEO_DIR8}" >> /tmp/minidlna.conf
 fi
 
 if [[ ! -z "${VIDEO_DIR9}" ]]; then
-  echo "media_dir=V,${VIDEO_DIR9}" >> /etc/minidlna.conf
+  echo "media_dir=V,${VIDEO_DIR9}" >> /tmp/minidlna.conf
 fi
 
 # Audio
 if [[ ! -z "${AUDIO_DIR1}" ]]; then
-  echo "media_dir=A,${AUDIO_DIR1}" >> /etc/minidlna.conf
+  echo "media_dir=A,${AUDIO_DIR1}" >> /tmp/minidlna.conf
 fi
 
 if [[ ! -z "${AUDIO_DIR2}" ]]; then
-  echo "media_dir=A,${AUDIO_DIR2}" >> /etc/minidlna.conf
+  echo "media_dir=A,${AUDIO_DIR2}" >> /tmp/minidlna.conf
 fi
 
 if [[ ! -z "${AUDIO_DIR3}" ]]; then
-  echo "media_dir=A,${AUDIO_DIR3}" >> /etc/minidlna.conf
+  echo "media_dir=A,${AUDIO_DIR3}" >> /tmp/minidlna.conf
 fi
 
 if [[ ! -z "${AUDIO_DIR4}" ]]; then
-  echo "media_dir=A,${AUDIO_DIR4}" >> /etc/minidlna.conf
+  echo "media_dir=A,${AUDIO_DIR4}" >> /tmp/minidlna.conf
 fi
 
 if [[ ! -z "${AUDIO_DIR5}" ]]; then
-  echo "media_dir=A,${AUDIO_DIR5}" >> /etc/minidlna.conf
+  echo "media_dir=A,${AUDIO_DIR5}" >> /tmp/minidlna.conf
 fi
 
 if [[ ! -z "${AUDIO_DIR6}" ]]; then
-  echo "media_dir=A,${AUDIO_DIR6}" >> /etc/minidlna.conf
+  echo "media_dir=A,${AUDIO_DIR6}" >> /tmp/minidlna.conf
 fi
 
 if [[ ! -z "${AUDIO_DIR7}" ]]; then
-  echo "media_dir=A,${AUDIO_DIR7}" >> /etc/minidlna.conf
+  echo "media_dir=A,${AUDIO_DIR7}" >> /tmp/minidlna.conf
 fi
 
 if [[ ! -z "${AUDIO_DIR8}" ]]; then
-  echo "media_dir=A,${AUDIO_DIR8}" >> /etc/minidlna.conf
+  echo "media_dir=A,${AUDIO_DIR8}" >> /tmp/minidlna.conf
 fi
 
 if [[ ! -z "${AUDIO_DIR9}" ]]; then
-  echo "media_dir=A,${AUDIO_DIR9}" >> /etc/minidlna.conf
+  echo "media_dir=A,${AUDIO_DIR9}" >> /tmp/minidlna.conf
 fi
 
 # Pictures
 if [[ ! -z "${PICTURES_DIR1}" ]]; then
-  echo "media_dir=P,${PICTURES_DIR1}" >> /etc/minidlna.conf
+  echo "media_dir=P,${PICTURES_DIR1}" >> /tmp/minidlna.conf
 fi
 
 if [[ ! -z "${PICTURES_DIR2}" ]]; then
-  echo "media_dir=P,${PICTURES_DIR2}" >> /etc/minidlna.conf
+  echo "media_dir=P,${PICTURES_DIR2}" >> /tmp/minidlna.conf
 fi
 
 if [[ ! -z "${PICTURES_DIR3}" ]]; then
-  echo "media_dir=P,${PICTURES_DIR3}" >> /etc/minidlna.conf
+  echo "media_dir=P,${PICTURES_DIR3}" >> /tmp/minidlna.conf
 fi
 
 if [[ ! -z "${PICTURES_DIR4}" ]]; then
-  echo "media_dir=P,${PICTURES_DIR4}" >> /etc/minidlna.conf
+  echo "media_dir=P,${PICTURES_DIR4}" >> /tmp/minidlna.conf
 fi
 
 if [[ ! -z "${PICTURES_DIR5}" ]]; then
-  echo "media_dir=P,${PICTURES_DIR5}" >> /etc/minidlna.conf
+  echo "media_dir=P,${PICTURES_DIR5}" >> /tmp/minidlna.conf
 fi
 
 if [[ ! -z "${PICTURES_DIR6}" ]]; then
-  echo "media_dir=P,${PICTURES_DIR6}" >> /etc/minidlna.conf
+  echo "media_dir=P,${PICTURES_DIR6}" >> /tmp/minidlna.conf
 fi
 
 if [[ ! -z "${PICTURES_DIR7}" ]]; then
-  echo "media_dir=P,${PICTURES_DIR7}" >> /etc/minidlna.conf
+  echo "media_dir=P,${PICTURES_DIR7}" >> /tmp/minidlna.conf
 fi
 
 if [[ ! -z "${PICTURES_DIR8}" ]]; then
-  echo "media_dir=P,${PICTURES_DIR8}" >> /etc/minidlna.conf
+  echo "media_dir=P,${PICTURES_DIR8}" >> /tmp/minidlna.conf
 fi
 
 if [[ ! -z "${PICTURES_DIR9}" ]]; then
-  echo "media_dir=P,${PICTURES_DIR9}" >> /etc/minidlna.conf
+  echo "media_dir=P,${PICTURES_DIR9}" >> /tmp/minidlna.conf
 fi
 
-/usr/sbin/minidlnad -P /cache/instance.pid -S -r
+/usr/sbin/minidlnad -P /run/minidlna.pid -f /tmp/minidlna.conf -S -r
+
+exit_code=$?
+
+rm -f /run/minidlna.pid
+
+exit $exit_code
