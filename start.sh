86d1b67aa6967cfc643d8468dc94e1d26165835d
Yehor Popovych <yehor@crossroad.io>
2020-05-10 21:23:42 +0300

    updated start script

--- start.sh
+++ start.sh
@@ -1,167 +1,182 @@
 #!/bin/sh
 
-if [[ ! -f "/cache/minidlna.conf" ]]; then
-  cp /etc/minidlna.conf /cache/
+INITIAL_CONF="/etc/minidlna.conf"
+PERSISTENT_CONF="/cache/minidlna.conf"
+TEMP_CONF="/tmp/minidlna.conf"
+PID_FILE="/run/minidlna.pid"
+
+## Copying initial config file to persistent storage
+if [[ ! -f "$PERSISTENT_CONF" ]]; then
+  cat "$INITIAL_CONF" > "$PERSISTENT_CONF"
 fi
 
-cp -f /cache/minidlna.conf /tmp/minidlna.conf
+## Copying persisten config file to the temp config file.
+cat "$PERSISTENT_CONF" > "$TEMP_CONF"
 
-echo "port=${TCP_PORT}" >> /tmp/minidlna.conf
-echo "friendly_name=${FRIENDLY_NAME}" >> /tmp/minidlna.conf
-echo "serial=${SERIAL}" >> /tmp/minidlna.conf
+## Updating temp config file with parameters from Docker ENV.
+echo "port=${TCP_PORT}" >> "$TEMP_CONF"
+echo "friendly_name=${FRIENDLY_NAME}" >> "$TEMP_CONF"
+echo "serial=${SERIAL}" >> "$TEMP_CONF"
 
 # All media dirs
 if [[ ! -z "${ALL_MEDIA_DIR1}" ]]; then
-  echo "media_dir=${ALL_MEDIA_DIR1}" >> /tmp/minidlna.conf
+  echo "media_dir=${ALL_MEDIA_DIR1}" >> "$TEMP_CONF"
 fi
 
 if [[ ! -z "${ALL_MEDIA_DIR2}" ]]; then
-  echo "media_dir=${ALL_MEDIA_DIR2}" >> /tmp/minidlna.conf
+  echo "media_dir=${ALL_MEDIA_DIR2}" >> "$TEMP_CONF"
 fi
 
 if [[ ! -z "${ALL_MEDIA_DIR3}" ]]; then
-  echo "media_dir=${ALL_MEDIA_DIR3}" >> /tmp/minidlna.conf
+  echo "media_dir=${ALL_MEDIA_DIR3}" >> "$TEMP_CONF"
 fi
 
 if [[ ! -z "${ALL_MEDIA_DIR4}" ]]; then
-  echo "media_dir=${ALL_MEDIA_DIR4}" >> /tmp/minidlna.conf
+  echo "media_dir=${ALL_MEDIA_DIR4}" >> "$TEMP_CONF"
 fi
 
 if [[ ! -z "${ALL_MEDIA_DIR5}" ]]; then
-  echo "media_dir=${ALL_MEDIA_DIR5}" >> /tmp/minidlna.conf
+  echo "media_dir=${ALL_MEDIA_DIR5}" >> "$TEMP_CONF"
 fi
 
 if [[ ! -z "${ALL_MEDIA_DIR6}" ]]; then
-  echo "media_dir=${ALL_MEDIA_DIR6}" >> /tmp/minidlna.conf
+  echo "media_dir=${ALL_MEDIA_DIR6}" >> "$TEMP_CONF"
 fi
 
 if [[ ! -z "${ALL_MEDIA_DIR7}" ]]; then
-  echo "media_dir=${ALL_MEDIA_DIR7}" >> /tmp/minidlna.conf
+  echo "media_dir=${ALL_MEDIA_DIR7}" >> "$TEMP_CONF"
 fi
 
 if [[ ! -z "${ALL_MEDIA_DIR8}" ]]; then
-  echo "media_dir=${ALL_MEDIA_DIR8}" >> /tmp/minidlna.conf
+  echo "media_dir=${ALL_MEDIA_DIR8}" >> "$TEMP_CONF"
 fi
 
 if [[ ! -z "${ALL_MEDIA_DIR9}" ]]; then
-  echo "media_dir=${ALL_MEDIA_DIR9}" >> /tmp/minidlna.conf
+  echo "media_dir=${ALL_MEDIA_DIR9}" >> "$TEMP_CONF"
 fi
 
 # Video
 if [[ ! -z "${VIDEO_DIR1}" ]]; then
-  echo "media_dir=V,${VIDEO_DIR1}" >> /tmp/minidlna.conf
+  echo "media_dir=V,${VIDEO_DIR1}" >> "$TEMP_CONF"
 fi
 
 if [[ ! -z "${VIDEO_DIR2}" ]]; then
-  echo "media_dir=V,${VIDEO_DIR2}" >> /tmp/minidlna.conf
+  echo "media_dir=V,${VIDEO_DIR2}" >> "$TEMP_CONF"
 fi
 
 if [[ ! -z "${VIDEO_DIR3}" ]]; then
-  echo "media_dir=V,${VIDEO_DIR3}" >> /tmp/minidlna.conf
+  echo "media_dir=V,${VIDEO_DIR3}" >> "$TEMP_CONF"
 fi
 
 if [[ ! -z "${VIDEO_DIR4}" ]]; then
-  echo "media_dir=V,${VIDEO_DIR4}" >> /tmp/minidlna.conf
+  echo "media_dir=V,${VIDEO_DIR4}" >> "$TEMP_CONF"
 fi
 
 if [[ ! -z "${VIDEO_DIR5}" ]]; then
-  echo "media_dir=V,${VIDEO_DIR5}" >> /tmp/minidlna.conf
+  echo "media_dir=V,${VIDEO_DIR5}" >> "$TEMP_CONF"
 fi
 
 if [[ ! -z "${VIDEO_DIR6}" ]]; then
-  echo "media_dir=V,${VIDEO_DIR6}" >> /tmp/minidlna.conf
+  echo "media_dir=V,${VIDEO_DIR6}" >> "$TEMP_CONF"
 fi
 
 if [[ ! -z "${VIDEO_DIR7}" ]]; then
-  echo "media_dir=V,${VIDEO_DIR7}" >> /tmp/minidlna.conf
+  echo "media_dir=V,${VIDEO_DIR7}" >> "$TEMP_CONF"
 fi
 
 if [[ ! -z "${VIDEO_DIR8}" ]]; then
-  echo "media_dir=V,${VIDEO_DIR8}" >> /tmp/minidlna.conf
+  echo "media_dir=V,${VIDEO_DIR8}" >> "$TEMP_CONF"
 fi
 
 if [[ ! -z "${VIDEO_DIR9}" ]]; then
-  echo "media_dir=V,${VIDEO_DIR9}" >> /tmp/minidlna.conf
+  echo "media_dir=V,${VIDEO_DIR9}" >> "$TEMP_CONF"
 fi
 
 # Audio
 if [[ ! -z "${AUDIO_DIR1}" ]]; then
-  echo "media_dir=A,${AUDIO_DIR1}" >> /tmp/minidlna.conf
+  echo "media_dir=A,${AUDIO_DIR1}" >> "$TEMP_CONF"
 fi
 
 if [[ ! -z "${AUDIO_DIR2}" ]]; then
-  echo "media_dir=A,${AUDIO_DIR2}" >> /tmp/minidlna.conf
+  echo "media_dir=A,${AUDIO_DIR2}" >> "$TEMP_CONF"
 fi
 
 if [[ ! -z "${AUDIO_DIR3}" ]]; then
-  echo "media_dir=A,${AUDIO_DIR3}" >> /tmp/minidlna.conf
+  echo "media_dir=A,${AUDIO_DIR3}" >> "$TEMP_CONF"
 fi
 
 if [[ ! -z "${AUDIO_DIR4}" ]]; then
-  echo "media_dir=A,${AUDIO_DIR4}" >> /tmp/minidlna.conf
+  echo "media_dir=A,${AUDIO_DIR4}" >> "$TEMP_CONF"
 fi
 
 if [[ ! -z "${AUDIO_DIR5}" ]]; then
-  echo "media_dir=A,${AUDIO_DIR5}" >> /tmp/minidlna.conf
+  echo "media_dir=A,${AUDIO_DIR5}" >> "$TEMP_CONF"
 fi
 
 if [[ ! -z "${AUDIO_DIR6}" ]]; then
-  echo "media_dir=A,${AUDIO_DIR6}" >> /tmp/minidlna.conf
+  echo "media_dir=A,${AUDIO_DIR6}" >> "$TEMP_CONF"
 fi
 
 if [[ ! -z "${AUDIO_DIR7}" ]]; then
-  echo "media_dir=A,${AUDIO_DIR7}" >> /tmp/minidlna.conf
+  echo "media_dir=A,${AUDIO_DIR7}" >> "$TEMP_CONF"
 fi
 
 if [[ ! -z "${AUDIO_DIR8}" ]]; then
-  echo "media_dir=A,${AUDIO_DIR8}" >> /tmp/minidlna.conf
+  echo "media_dir=A,${AUDIO_DIR8}" >> "$TEMP_CONF"
 fi
 
 if [[ ! -z "${AUDIO_DIR9}" ]]; then
-  echo "media_dir=A,${AUDIO_DIR9}" >> /tmp/minidlna.conf
+  echo "media_dir=A,${AUDIO_DIR9}" >> "$TEMP_CONF"
 fi
 
 # Pictures
 if [[ ! -z "${PICTURES_DIR1}" ]]; then
-  echo "media_dir=P,${PICTURES_DIR1}" >> /tmp/minidlna.conf
+  echo "media_dir=P,${PICTURES_DIR1}" >> "$TEMP_CONF"
 fi
 
 if [[ ! -z "${PICTURES_DIR2}" ]]; then
-  echo "media_dir=P,${PICTURES_DIR2}" >> /tmp/minidlna.conf
+  echo "media_dir=P,${PICTURES_DIR2}" >> "$TEMP_CONF"
 fi
 
 if [[ ! -z "${PICTURES_DIR3}" ]]; then
-  echo "media_dir=P,${PICTURES_DIR3}" >> /tmp/minidlna.conf
+  echo "media_dir=P,${PICTURES_DIR3}" >> "$TEMP_CONF"
 fi
 
 if [[ ! -z "${PICTURES_DIR4}" ]]; then
-  echo "media_dir=P,${PICTURES_DIR4}" >> /tmp/minidlna.conf
+  echo "media_dir=P,${PICTURES_DIR4}" >> "$TEMP_CONF"
 fi
 
 if [[ ! -z "${PICTURES_DIR5}" ]]; then
-  echo "media_dir=P,${PICTURES_DIR5}" >> /tmp/minidlna.conf
+  echo "media_dir=P,${PICTURES_DIR5}" >> "$TEMP_CONF"
 fi
 
 if [[ ! -z "${PICTURES_DIR6}" ]]; then
-  echo "media_dir=P,${PICTURES_DIR6}" >> /tmp/minidlna.conf
+  echo "media_dir=P,${PICTURES_DIR6}" >> "$TEMP_CONF"
 fi
 
 if [[ ! -z "${PICTURES_DIR7}" ]]; then
-  echo "media_dir=P,${PICTURES_DIR7}" >> /tmp/minidlna.conf
+  echo "media_dir=P,${PICTURES_DIR7}" >> "$TEMP_CONF"
 fi
 
 if [[ ! -z "${PICTURES_DIR8}" ]]; then
-  echo "media_dir=P,${PICTURES_DIR8}" >> /tmp/minidlna.conf
+  echo "media_dir=P,${PICTURES_DIR8}" >> "$TEMP_CONF"
 fi
 
 if [[ ! -z "${PICTURES_DIR9}" ]]; then
-  echo "media_dir=P,${PICTURES_DIR9}" >> /tmp/minidlna.conf
+  echo "media_dir=P,${PICTURES_DIR9}" >> "$TEMP_CONF"
 fi
 
-/usr/sbin/minidlnad -P /run/minidlna.pid -f /tmp/minidlna.conf -S -r
 
-exit_code=$?
+## Sometimes PID file can exist after restart. Cleaning.
+if [[ -f "$PID_FILE" ]]; then
+  pid=$(cat "$PID_FILE")
+  if [[ -n "$(ps -p $pid -o pid=)" ]]; then
+    echo "Another MiniDLNA instance is running: $pid. Exiting..."
+    exit 1
+  else
+    echo "Removing uncleaned pid file: $PID_FILE"
+    rm -f "$PID_FILE"
+  fi
+fi
 
-rm -f /run/minidlna.pid
-
-exit $exit_code
+exec /usr/sbin/minidlnad -P "$PID_FILE" -f "$TEMP_CONF" -S -r
